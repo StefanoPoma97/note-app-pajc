c8340e40b2ea98f0055354cca40b0f038216030f
StefanoPoma97 <s.poma001@studenti.unibs.it>
2018-07-31 12:47:14 +0200

    creato package view

---
+++ src/it/unibs/pajc/note/view/LoginView.java
@@ -0,0 +1,135 @@
+package it.unibs.pajc.note.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import it.unibs.pajc.note.model.User;
+import javax.swing.JFormattedTextField;
+import javax.swing.JButton;
+
+public class LoginView extends JPanel {
+	
+	//stringa per comunicare informazioni al main form
+	private String out;
+	//user creato o per il login
+	private User user = null;
+	
+	private JTextField textFieldName;
+	private JTextField textFieldPass;
+	
+	
+	private ArrayList<ActionListener> subscriberOnList = new ArrayList<>();
+	
+	//metodo per aggiungere Actionlistener alla lista
+	public void addActionList (ActionListener l)
+	{
+		subscriberOnList.add(l);
+	}	
+	
+	//fire della lista 
+	private void fireList (ActionEvent e)
+	{
+		e.setSource(this);
+		for (ActionListener l: subscriberOnList)
+		{
+			l.actionPerformed(e);
+		}
+	}
+	
+	
+	public void Login (ActionEvent e)
+	{
+		System.out.println("richiamo il metodo fire");
+		fireList(e);
+		user= new User (textFieldName.getText(), textFieldPass.getText());
+		System.out.println("ho creato l'utente");
+	}
+	
+	public void CreateAccount (ActionEvent e)
+	{
+		System.out.println("richiamo il metodo fire");
+		fireList(e);
+		user= new User (textFieldName.getText(), textFieldPass.getText());
+		System.out.println("ho creato l'utente");
+	}
+	
+	public User getUser(){
+		return user;
+	}
+	
+	public String getAction(){
+		return out;
+	}
+	
+
+	
+	public LoginView() {
+		setLayout(null);
+		
+		textFieldName = new JTextField();
+		textFieldName.setBounds(192, 96, 122, 35);
+		textFieldName.setHorizontalAlignment(SwingConstants.CENTER);
+		add(textFieldName);
+		textFieldName.setColumns(10);
+		
+		textFieldPass = new JTextField();
+		textFieldPass.setHorizontalAlignment(SwingConstants.CENTER);
+		textFieldPass.setColumns(10);
+		textFieldPass.setBounds(192, 142, 122, 35);
+		add(textFieldPass);
+		
+		
+		
+		JFormattedTextField textName = new JFormattedTextField();
+		textName.setHorizontalAlignment(SwingConstants.CENTER);
+		textName.setEditable(false);
+		textName.setText("Name");
+		textName.setBounds(102, 96, 66, 35);
+		add(textName);
+		
+		JFormattedTextField textPassword = new JFormattedTextField();
+		textPassword.setText("Password");
+		textPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		textPassword.setEditable(false);
+		textPassword.setBounds(102, 142, 66, 35);
+		add(textPassword);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(102, 230, 99, 23);
+		add(btnLogin);
+		
+		JButton btnNewAccount = new JButton("New Account");
+		btnNewAccount.setBounds(225, 230, 102, 23);
+		add(btnNewAccount);
+		
+		
+		
+		
+		//action listener
+		
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("riciamo il metodo login");
+				Login(e);
+				System.out.println("out = login");
+				out="login";
+			}
+		});
+		
+		btnNewAccount.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("richiamo il metodo create");
+				CreateAccount(e);
+				System.out.println("out = create");
+				out="create";
+				
+			}
+		});
+		
+		
+
+	}
+}
---
+++ src/it/unibs/pajc/note/view/MainForm.java
@@ -0,0 +1,70 @@
+package it.unibs.pajc.note.view;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import it.unibs.pajc.note.model.User;
+
+import java.awt.BorderLayout;
+
+public class MainForm {
+
+	private JFrame frame;
+	
+	private String in= null;
+	private User user=null;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainForm window = new MainForm();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainForm() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		LoginView loginView = new LoginView();
+		frame.getContentPane().add(loginView, BorderLayout.CENTER);
+		
+		
+		
+		
+		
+		
+		
+		loginView.addActionList(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				in=loginView.getAction();
+				user= loginView.getUser();
+				System.out.println("main : arrivato stringa "+in);
+				System.out.println("main : arrivato utente "+user.toString());
+			}
+		});
+	}
+
+}
