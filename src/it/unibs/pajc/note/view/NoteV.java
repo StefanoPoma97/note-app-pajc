84734f9464d8ce59955ca59b019ea0c5a5d8dbe5
StefanoPoma97 <s.poma001@studenti.unibs.it>
2018-08-01 15:33:16 +0200

    classe NoteView

--- src/it/unibs/pajc/note/view/NoteV.java
+++ src/it/unibs/pajc/note/view/NoteV.java
@@ -1,6 +1,7 @@
 package it.unibs.pajc.note.view;
 
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
 
 public class NoteV extends JPanel {
 
@@ -8,6 +9,13 @@ public class NoteV extends JPanel {
 	 * Create the panel.
 	 */
 	public NoteV() {
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel contentList = new JPanel();
+		add(contentList);
+		
+		JPanel contentButton = new JPanel();
+		add(contentButton, BorderLayout.NORTH);
 
 	}
 
