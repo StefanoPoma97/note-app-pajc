57602acef6e8f6a0adc844ba1b2dbbb0cc70ff11
StefanoPoma97 <s.poma001@studenti.unibs.it>
2018-08-03 12:13:12 +0200

    convertiti tutti i JButton in JPanel. iniziato a riempire i vari pannelli con i componenti utili per il funzionamento dell'interaccia

--- src/it/unibs/pajc/note/view/NoteView.java
+++ src/it/unibs/pajc/note/view/NoteView.java
@@ -4,24 +4,42 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-
 import javax.swing.JFormattedTextField;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.BoxLayout;
+import javax.swing.JToolBar;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JToggleButton;
+import javax.swing.JSpinner;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+
 
 public class NoteView extends JPanel {
+	//Pannelli contenitivi
 	private JPanel contentList;
 	private JPanel contentButton;
 	private JPanel contentNote;
 	private JPanel contentModify;
-	private JButton btnNewButton;
-	private JButton btnNewButton_1;
-	private JButton btnNewButton_2;
-	private JButton btnNewButton_3;
+	private JPanel contentInfo;
 	private GridBagConstraints gc_1;
+	private GridBagConstraints gc_2;
+	private GridBagConstraints gc_3;
+	private GridBagConstraints gc_4;
+	
+	//componenti interni
+	private JButton btnRefresh;
+	private JButton btnNewNote;
+	private JButton btnExplore;
+	private JButton btnPin;
+	private JComboBox comboFilter;
+	private JComboBox comboLabels;
+
 
 	/**
 	 * Create the panel.
@@ -31,59 +49,105 @@ public class NoteView extends JPanel {
 		
 	}
 	
+	/**
+	 * metodo per la creazione dell'interfazzia utilizzando il GridBagLayout
+	 */
 	private void build(){
+		//setta il tipo di layout da utilizzare
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		this.setLayout(gridBagLayout);
 		
+		//row 0
+			//col 0  Pannello Buttons
 		GridBagConstraints gc = new GridBagConstraints();
-		this.btnNewButton = new JButton();
-		this.btnNewButton.setPreferredSize(new Dimension(100, 30));
-		gc.weightx=0;
-//		gc.weighty=0.001;
+		gc.insets = new Insets(0, 0, 5, 5);
+		this.contentButton = new JPanel();
+		contentButton.setBackground(Color.LIGHT_GRAY);
+		this.contentButton.setPreferredSize(new Dimension(300, 50));
+		gc.weightx=0; //crescita dello 0% lungo l'asse X
 		gc.gridx = 0;
 		gc.gridy = 0;
-		gc.fill=GridBagConstraints.BOTH;
-//		gc.insets = new Insets(10, 10, 0, 0);
+		gc.fill=GridBagConstraints.BOTH; //riempie tutto lo spazio a disposizione in tutte le direzioni
 		gc.anchor = GridBagConstraints.SOUTHWEST;
-		this.add(this.btnNewButton, gc);
+		this.add(this.contentButton, gc);
+		contentButton.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
+		btnRefresh = new JButton("Refresh");
+		contentButton.add(btnRefresh);
+		btnNewNote = new JButton("+");
+		contentButton.add(btnNewNote);
+		
+		comboLabels = new JComboBox();
+		comboLabels.setModel(new DefaultComboBoxModel(new String[] {"", "Riunione", "Memo", "Link"}));
+		comboLabels.setToolTipText("");
+		contentButton.add(comboLabels);
+		
+		comboFilter = new JComboBox();
+		comboFilter.setModel(new DefaultComboBoxModel(new String[] {"", "Titolo", "Autore", "Like"}));
+		contentButton.add(comboFilter);
+		comboFilter.setToolTipText("");
+		
+			//col 1
 		gc_1 = new GridBagConstraints();
-		gc_1.anchor = GridBagConstraints.WEST;
-		this.btnNewButton_1 = new JButton();
-		this.btnNewButton_1.setPreferredSize(new Dimension(100, 30));
-		gc.weightx=0;
-		gc_1.weighty=1.0;
-		gc_1.gridx = 0;
-		gc_1.gridy = 1;
-		gc_1.fill=GridBagConstraints.BOTH;
-				;
-		this.add(this.btnNewButton_1, gc_1);
+		gc_1.insets = new Insets(0, 0, 5, 0);
+		this.contentInfo = new JPanel();
+		this.contentInfo.setPreferredSize(new Dimension(100, 50));
+		contentInfo.setBackground(new Color(0, 0, 255));
+		gc_1.weightx=1; //cresce completamente 100% lungo X
+		gc_1.gridx = 1;
+		gc_1.gridy = 0;
+		gc_1.fill=GridBagConstraints.HORIZONTAL; //occupa lo spazio a disposizione solo in orizzontale
+		gc_1.anchor = GridBagConstraints.LINE_START; //si fissa al inizio della cella
+		this.add(this.contentInfo, gc_1);
 		
-		gc = new GridBagConstraints();
-		this.btnNewButton_2 = new JButton();
-//		this.btnNewButton_1.setPreferredSize(new Dimension(100, 30));
-		gc.weightx=1;
-		gc.weighty=1.0;
-		gc.gridx = 1;
-		gc.gridy = 1;
-		gc.fill=GridBagConstraints.BOTH;
-//		gc.insets = new Insets(10, 10, 0, 0);
-		gc.anchor = GridBagConstraints.LINE_START;
-				;
-		this.add(this.btnNewButton_2, gc);
+		btnExplore = new JButton("Esplora");
+		contentInfo.add(btnExplore);
 		
-		gc = new GridBagConstraints();
-		this.btnNewButton_3 = new JButton();
-		this.btnNewButton_3.setPreferredSize(new Dimension(100, 30));
-		gc.weightx=0.001;
-//		gc.weighty=1.0;
-		gc.gridx = 1;
-		gc.gridy = 2;
-		gc.fill=GridBagConstraints.HORIZONTAL;
+		//row 1
+			//col 0
+		gc_2 = new GridBagConstraints();
+		gc_2.insets = new Insets(0, 0, 5, 5);
+		gc_2.anchor = GridBagConstraints.WEST;
+		this.contentList = new JPanel();
+		this.contentList.setPreferredSize(new Dimension(300, 30));
+		gc_2.weightx=0;
+		gc_2.weighty=1.0;
+		gc_2.gridx = 0;
+		gc_2.gridy = 1;
+		gc_2.fill=GridBagConstraints.BOTH;
+		this.add(this.contentList, gc_2);
+		
+			//col 1
+		gc_3 = new GridBagConstraints();
+		gc_3.insets = new Insets(0, 0, 5, 0);
+		this.contentNote = new JPanel();
+		contentNote.setBackground(new Color(240, 230, 140));
+		gc_3.weightx=1;
+		gc_3.weighty=1.0;
+		gc_3.gridx = 1;
+		gc_3.gridy = 1;
+		gc_3.fill=GridBagConstraints.BOTH;
 //		gc.insets = new Insets(10, 10, 0, 0);
-		gc.anchor = GridBagConstraints.LINE_START;
-				;
-		this.add(this.btnNewButton_3, gc);
+		gc_3.anchor = GridBagConstraints.LINE_START;
+		this.add(this.contentNote, gc_3);
+		
+		//row 2
+			//col 0 vuota
+			//col 1
+		gc_4 = new GridBagConstraints();
+		gc_4.insets = new Insets(0, 0, 5, 0);
+		this.contentModify = new JPanel();
+		contentModify.setBackground(new Color(255, 255, 0));
+		this.contentModify.setPreferredSize(new Dimension(100, 50));
+		gc_4.weightx=0.001;
+		gc_4.gridx = 1;
+		gc_4.gridy = 2;
+		gc_4.fill=GridBagConstraints.HORIZONTAL;
+//		gc.insets = new Insets(10, 10, 0, 0);
+		gc_4.anchor = GridBagConstraints.LINE_START;
+		this.add(this.contentModify, gc_4);
+		btnPin = new JButton("Pin");
+		contentModify.add(btnPin);
 		
 		
 		
